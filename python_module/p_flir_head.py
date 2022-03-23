27cd0777eed30635957f46de46d1d2508fd0fc06
Dieter Vansteenwegen <github@vansteenwegen.org>
2022-03-23 14:56:20 +0100

    Python modules layout

---
+++ python_module/p_flir_head.py
@@ -0,0 +1,29 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'Dieter Vansteenwegen'
+__project__ = 'XXX'
+__project_link__ = 'https://www.vansteenwegen.org'
+
+import logging
+
+
+def initialize_logger() -> logging.Logger:
+    """Set up logger
+    If the module is ran as a module, name logger accordingly as a sublogger.
+    Returns:
+        logging.Logger: logger instance
+    """
+    if __name__ == '__main__':
+        return logging.getLogger('{}'.format(__name__))
+    else:
+        return logging.getLogger('__main__.{}'.format(__name__))
+
+
+class ADCNotImplemented(NotImplementedError):
+    """Info here."""
+    pass
+
+
+class pPTHEAD:
+    pass
