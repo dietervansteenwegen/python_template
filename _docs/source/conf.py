675c964a176c6a8663318b8852ae6267e34d4f35
Dieter Vansteenwegen <github@vansteenwegen.org>
2022-03-23 14:58:19 +0100

    better version number, no copyright, excl. setup.rst

--- _docs/source/conf.py
+++ _docs/source/conf.py
@@ -1,11 +1,10 @@
+# -*- coding: utf-8 -*-
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-
 # -- Path setup --------------------------------------------------------------
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -17,12 +16,11 @@ sys.path.insert(0, os.path.abspath('../..'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'Panthyr FLIR PTU-D48e'
-copyright = '2021, Dieter Vansteenwegen'
+project = 'Panthyr XXXXXXX'
 author = 'Dieter Vansteenwegen'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1b'
+release = '0.0.1-beta.1'
 
 # -- General configuration ---------------------------------------------------
 
@@ -44,7 +42,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['setup.rst']
 
 # -- Options for HTML output -------------------------------------------------
 
