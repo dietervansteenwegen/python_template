d56577dd6041c7e5407ffb150b6228839501e2c4
dietervansteenwegen <github@vansteenwegen.org>
2023-12-08 13:44:42 +0100

    Cleanup

--- src/python_module/__init__.py
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/python3
-# -*- coding: utf-8 -*-
-# vim: ts=4:sw=4:expandtab:cuc:autoindent:ignorecase:colorcolumn=99
-
-__author__ = 'Dieter Vansteenwegen'
-__project__ = 'REPLACE_WITH_PROJECT_NAME'
-__project_link__ = 'https://www.vansteenwegen.org'
